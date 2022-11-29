b01dda9e39d9af7f48a77103b96c76c4f30fb020
unknown <priyankchavda601@gmail.com>
2022-11-29 19:30:57 +0530

    codeForPrimenumbers

---
+++ NewCode.js
@@ -0,0 +1,13 @@
+var x=3;
+var count=0;
+for (var i=1;i<=x;i++){
+  if (x%i==0){
+    count+=1;
+  }
+}
+if (count==0){
+  console.log(x, "is a prime number");
+}
+else {
+  console.log(x, "is not a prime number");
+}
